cd788a38fe3ff132f64343fa6bbaccc67552801a
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2019-03-27 19:06:30 -0500

    Fragments (Part 3)

--- app/src/main/java/com/co/androidappdemokotlin/MainActivity.kt
+++ app/src/main/java/com/co/androidappdemokotlin/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.co.androidappdemokotlin
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NewMovieFragment.OnFragmentInteractionListener {
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.flContent, NewMovieFragment.newInstance())
+            transaction.commit()
+        }
     }
 }
