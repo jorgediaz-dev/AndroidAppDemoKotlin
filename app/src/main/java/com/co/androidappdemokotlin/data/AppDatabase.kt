1c004635f92687fd8bb89b9ce9950b2a68543740
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2019-09-01 23:46:39 -0500

    Singleton instance

--- app/src/main/java/com/co/androidappdemokotlin/data/AppDatabase.kt
+++ app/src/main/java/com/co/androidappdemokotlin/data/AppDatabase.kt
@@ -1,10 +1,31 @@
 package com.co.androidappdemokotlin.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = arrayOf(Movie::class), version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieDao() : MovieDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder((context.applicationContext),
+                        AppDatabase::class.java, "movie-database")
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
 }
 
