f8a190fe8b42074f544c6c21c29f0d11482bafb8
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2019-09-01 23:47:31 -0500

    First Coroutines implementation

--- app/src/main/java/com/co/androidappdemokotlin/NewMovieViewModel.kt
+++ app/src/main/java/com/co/androidappdemokotlin/NewMovieViewModel.kt
@@ -2,7 +2,21 @@ package com.co.androidappdemokotlin
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.co.androidappdemokotlin.data.AppDatabase
+import com.co.androidappdemokotlin.data.Movie
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class NewMovieViewModel(application : Application) : AndroidViewModel(application) {
 
+    private val mDb: AppDatabase? = AppDatabase.getInstance(application)
+
+    fun storeMovie(title: String) {
+        val movie = Movie()
+        movie.name = title
+
+        GlobalScope.launch {
+        mDb?.movieDao()?.insert(movie)
+        }
+    }
 }
